978cc0942077b845f2fcd13b52295c81df91e145
Rodrigo Araujo <rodrigoufu@gmail.com>
2021-03-22 19:44:11 -0300

    Adicionado serviço de georefenciamento no Eureka e Api Gateway.

--- src/main/java/br/pucminas/pos/tcc/mimg/MimgApplication.java
+++ src/main/java/br/pucminas/pos/tcc/mimg/MimgApplication.java
@@ -2,11 +2,12 @@ package br.pucminas.pos.tcc.mimg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MimgApplication {
 
 	public static void main(String[] args) {
